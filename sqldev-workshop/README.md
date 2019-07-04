7491dc13426ec6698a413b7af1bb2892b1902bed
sysadmin <sysadmin@domain.com>
2019-07-04 02:06:43 -0500

    9no commit

--- sqldev-workshop/README.md
+++
@@ -1,3 +0,0 @@
-# sqldev-workshop
-
-This repository is used to explain GIT and how it integrates with SQL Developer
--- sqldev-workshop/employees_tab.sql
+++
@@ -1,7 +0,0 @@
-CREATE TABLE employees
-( employee_id NUMBER PRIMARY KEY
-, first_name  VARCHAR2(25)
-, last_name   VARCHAR2(25)
-, hire_date   DATE
-, salary      NUMBER
-);
